b01f174202723341749383476fb606556a7633b6
Douglas Eduardo <douglas.patusco@gmail.com>
2023-08-29 13:17:59 -0300

    Final

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,8 @@
 <script setup>
-    import { reactive } from 'vue';
     import Header from './components/Header.vue'
     import Main from './components/Main.vue'
     import Result from './components/Result.vue'
-
-    const estado = reactive({
-        numero1: '',
-        numero2: '',
-        operador: 'adicao',
-        resultado: 0,
-    })
-
-    const adicao = () => { 
-        const resultNumber = parseInt(estado.numero1) + parseInt(estado.numero2);
-        return resultNumber
-    }
-
-    const subtracao = () => {
-        const resultNumber = parseInt(estado.numero1) - parseInt(estado.numero2);
-        return resultNumber
-    }
-
-    const multiplicacao = () => {
-        const resultNumber = parseInt(estado.numero1) * parseInt(estado.numero2);
-        return resultNumber
-    }
-
-    const divisao = () => {
-        const resultNumber = parseInt(estado.numero1) / parseInt(estado.numero2);
-        return resultNumber
-}
-
-    const operacao = () => {
-        const { operador } = estado;
-
-        switch(operador) {
-            case 'adicao': {
-            return adicao();
-            }
-            case 'subtracao': {
-            return subtracao();
-            }
-            case 'multiplicacao': {
-            return multiplicacao();
-            }
-            case 'divisao': {
-            return divisao();
-            }
-        }
-    }
-
-    console.log(estado.resultado)
+    import { operacao, estado} from './scripts/Scripts.vue'
 </script>
 
 <template>
---
+++ src/scripts/Scripts.vue
@@ -0,0 +1,50 @@
+<script>
+    import { reactive } from 'vue';
+
+    export const estado = reactive({
+        numero1: '',
+        numero2: '',
+        operador: 'adicao',
+        resultado: '',
+    })
+
+    export const adicao = () => { 
+        const resultNumber = parseInt(estado.numero1) + parseInt(estado.numero2);
+        return resultNumber
+    }
+
+    export const subtracao = () => {
+        const resultNumber = parseInt(estado.numero1) - parseInt(estado.numero2);
+        return resultNumber
+    }
+
+    export const multiplicacao = () => {
+        const resultNumber = parseInt(estado.numero1) * parseInt(estado.numero2);
+        return resultNumber
+    }
+
+    export const divisao = () => {
+        const resultNumber = parseInt(estado.numero1) / parseInt(estado.numero2);
+        return resultNumber
+}
+
+    export const operacao = () => {
+        const { operador } = estado;
+
+        switch(operador) {
+            case 'adicao': {
+            return adicao();
+            }
+            case 'subtracao': {
+            return subtracao();
+            }
+            case 'multiplicacao': {
+            return multiplicacao();
+            }
+            case 'divisao': {
+            return divisao();
+            }
+        }
+    }
+
+</script>
